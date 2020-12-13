560ab6323712cda4f41984879435bc3c341f52b1
Callum <cnsc201@exeter.ac.uk>
2020-12-13 20:44:09 +0000

    inital learning algorithms setup and planned Training model solves successfully

--- main.py
+++ main.py
@@ -21,12 +21,44 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
 
+def basicdatacheck():
+    print(dataset.shape)
+    print(dataset.head(20))
+    print(dataset.describe())
+    print(dataset.groupby('class').size())
+
+
+def basicvisulatisations():
+    dataset.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
+    dataset.hist()
+    scatter_matrix(dataset)
+    pyplot.show()
+
+
 def main():
+    array = dataset.values
+    X = array[:, 0:4]
+    y = array[:, 4]
+    X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+    # algorithms
+    models = [('LR', LogisticRegression(solver='liblinear', multi_class='ovr')), ('LDA', LinearDiscriminantAnalysis()),
+              ('KNN', KNeighborsClassifier()), ('CART', DecisionTreeClassifier()), ('NB', GaussianNB()),
+              ('SVM', SVC(gamma='auto'))]
+    # evaluate in turn
+    results = []
+    names = []
+    for name, model in models:
+        kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+        cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+        results.append(cv_results)
+        names.append(name)
+        print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
 
 
 if __name__ == '__main__':
