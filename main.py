3d5273ac7bdc849c50cdfb1d4177423890f24ba6
Callum <cnsc201@exeter.ac.uk>
2020-12-13 12:34:33 +0000

    changed to allow power script

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def power(num, x=1):
+    result = 1
+    for i in range(x):
+        result = result*num
+    return result
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print('I have made changes to this file')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    number = int(input('Please provide a number...'))
+    indice = int(input('Please provide a power...'))
+    print(power(number, indice))
+2
