e523dc1c31f6a3278d8d7563918550d4f637702a
Callum <cnsc201@exeter.ac.uk>
2020-12-13 20:24:10 +0000

    import all packages and read dataset

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-def power(num, x=1):
-    result = 1
-    for i in range(x):
-        result = result*num
-    return result
+import math
+import numpy
+import scipy
+import matplotlib
+import pandas
+import sklearn
+
+# Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+
+def main():
 
 
 if __name__ == '__main__':
-    number = int(input('Please provide a number...'))
-    indice = int(input('Please provide a power...'))
-    print(power(number, indice))
-2
+    main()
